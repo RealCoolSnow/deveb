0b3a8a4bb0483d1c004166894403d41c016478d3
Naz Esmaeilian <nazesmaeilian@Nazs-MacBook-Air.local>
2022-05-23 16:24:45 +0200

    three

--- src/components/three/scene.js
+++ src/components/three/scene.js
@@ -1,16 +1,23 @@
 import React, {useEffect, useRef, useState} from "react";
 import {Canvas} from "@react-three/fiber"
+import {MeshDistortMaterial, Sphere} from "@react-three/drei"
 const Scene = () => {
 
     return( <Canvas className="can">
-                <pointLight position={[15,60,100]}/>
-                <mesh>
-                <sphereGeometry args={[1, 16, 16]} />
-                <meshStandardMaterial color="hotpink" />
-                </mesh>
-                <mesh>
+                {/* <pointLight position={[15,60,100]}/> */}
+                <ambientLight intensity={.5}/>
+                <directionalLight intensity={1} position={[-2,5,2]}/>
+                {/* <mesh> */}
+                <Sphere visible args={[1, 36, 36]} >
+                 <MeshDistortMaterial attach="material" color="#ff82d1" distort={0.6} speed={6}/>
+
+                </Sphere>
+                
+                {/* <meshStandardMaterial color="hotpink" /> */}
+                {/* </mesh> */}
+                {/* <mesh>
                     <planeBufferGeometry args={[15, 32]}/>
-                </mesh>
+                </mesh> */}
           </Canvas>)
 }
 
